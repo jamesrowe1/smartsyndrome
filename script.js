ad2b7122d6e1d26f0b884d6f6a529b7d712bd736
James Rowe <jamesrowe@outlook.com>
2020-08-01 22:29:21 -0400

    made new lines work

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ prizeWheel = $("#prizewheel");
 prizeWheelResult = $("#prizeWheelResult");
 
 const possiblePrize = [
-  "More time off! Becoming so much work must have been exhausting. Take some time off.",
+  "More time off! Doing so much work to win must have been exhausting. Take some time off.",
   "Extra Shifts! You're just so good we want you to work more!",
-  "MONEY! 'Nuff said",
+  "MONEY! 'Nuff said.",
   "Free Food! It how they bribe you to keep working harder!",
   "Therapy Sessions! I'm a website, and I need it. I don't know how you get by.",
   "A dedicated secretary to do your paperwork! This would actually make you try to win every week wouldn't it?",
@@ -13,4 +13,7 @@ const possiblePrize = [
 prizeWheel.on("click", function () {
   let chooser = Math.floor(Math.random() * possiblePrize.length);
   prizeWheelResult.text(possiblePrize[chooser]);
+  prizeWheelResult.append(
+    "<br /><br />Disclaimer: This prize is merely speculation and is unlikely to be correct. As such, this app should be used and trusted by no one"
+  );
 });
