67cc107dd9edddbddd927938892bd31286c23a84
James Rowe <jamesrowe@outlook.com>
2020-08-01 21:51:29 -0400

    random array

---
+++ script.js
@@ -0,0 +1,8 @@
+const possiblePrize = [
+  "More time off! Becoming so much work must have been exhausting. Take some time off.",
+  "Extra Shifts! You're just so good you need to work more!",
+  "MONEY! 'Nuff said",
+  "Free Food! It how they bribe you to keep working harder!",
+  "Therapy Sessions! We all need it at this point.",
+  "A dedicated secretary to do your paperwork! This would actually make you try to win every week wouldn't it?",
+];
