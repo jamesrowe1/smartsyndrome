9fcfd8bf5c35ecab99f123af8e008ad9e964a8b0
James Rowe <jamesrowe@outlook.com>
2020-08-01 22:02:34 -0400

    prizewheel

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+prizeWheel = $("#prizewheel");
+prizeWheelResult = $("#prizeWheelResult");
+
 const possiblePrize = [
   "More time off! Becoming so much work must have been exhausting. Take some time off.",
   "Extra Shifts! You're just so good you need to work more!",
@@ -6,3 +9,8 @@ const possiblePrize = [
   "Therapy Sessions! We all need it at this point.",
   "A dedicated secretary to do your paperwork! This would actually make you try to win every week wouldn't it?",
 ];
+
+prizeWheel.on("click", function () {
+  let chooser = Math.floor(Math.random() * possiblePrize.length);
+  prizeWheelResult.text(possiblePrize(chooser));
+});
